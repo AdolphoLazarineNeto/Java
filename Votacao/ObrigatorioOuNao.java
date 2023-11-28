e5f3a20d23aeaf67c1367514b0a9d087a91c447e
netinhop <adolphoneto@live.com>
2023-11-27 23:32:56 -0300

    initial commit

--- Votacao/ObrigatorioOuNao.java
+++ Votacao/ObrigatorioOuNao.java
@@ -6,18 +6,19 @@ public class ObrigatorioOuNao {
 
     public static void main(String[] args) {
     System.out.print("Quer saber se é obrigatorio o seu voto para presidente? Me fale o ano que nasceu!");
-    Scanner t = new Scanner(System.in);
-    int nasc = t.nextInt();
-    int i = 2023 - nasc;
-    if (i<16){
-        System.out.println("Não vota");
-    } else {
-    if ((i>=16 && i<18)||(i>70)){
-        System.out.print("Opcional");
-    } else {
-        System.out.print("Obrigatorio");       
+    try (Scanner t = new Scanner(System.in)) {
+        int nasc = t.nextInt();
+        int i = 2023 - nasc;
+        if (i<16){
+            System.out.println("Não vota");
+        } else {
+        if ((i>=16 && i<18)||(i>70)){
+            System.out.print("Opcional");
+        } else {
+            System.out.print("Obrigatorio");       
+        }
+         }
     }
-     }
     }
     
 }
